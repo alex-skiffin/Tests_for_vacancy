4b89bb8f20c0e1e7238268c642df869d67dff3e6
Alex Skiffin <alex@skiffin.ru>
2013-05-24 15:27:25 +0600

    new 2's test
    Тест на поиск множителей и тест на поиск сумм кватратов в таблице

---
+++ test_2.cs
@@ -0,0 +1,51 @@
+﻿namespace ивс_тест_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            //строка хранения результата
+            string vyvod = string.Empty;
+
+            int vhod;
+            System.Console.WriteLine("Введите число: ");
+
+            //получаем число
+            if (!int.TryParse(System.Console.ReadLine(), out vhod)) return;
+
+            //проверяем делится ли введенное число на 2
+            while ((vhod % 2) == 0)
+            {
+                vhod = vhod / 2;
+                vyvod += "2*";
+            }
+
+            //первый нечетный множитель
+            int mnozh = 3;
+
+            //максимально допустимый множитель
+            int c = (int)System.Math.Sqrt(vhod) + 1;
+
+            //перебираем возможные множители
+            while (mnozh < c)
+            {
+                //проверяем множитель
+                if ((vhod % mnozh) == 0)
+                {
+                    vyvod += mnozh.ToString() + "*";
+                    vhod = vhod / mnozh;
+                    c = (int)System.Math.Sqrt(vhod) + 1;
+                }
+                //если множитель не подходит - переходим к следующему нечетному числу
+                else
+                {
+                    mnozh += 2;
+                }
+            }
+            vyvod += vhod.ToString();
+            System.Console.WriteLine(vyvod);
+            System.Console.Write("Нажмите любую клавишу для закрытия.");
+            System.Console.ReadKey(true);
+        }
+    }
+}
---
+++ test_3.cs
@@ -0,0 +1,72 @@
+﻿namespace ивс_тест_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int squreSize, tableSize;
+
+            //получаем значение размера таблицы
+            System.Console.WriteLine("Введите размер таблицы (не больше 100): ");
+            if (!int.TryParse(System.Console.ReadLine(), out tableSize)) return;
+
+            //проверяем что квадрат не больше таблицы
+            if (tableSize > 100)
+            {
+                System.Console.WriteLine("Размер таблицы не должен быть превышать 100!");
+
+                System.Console.WriteLine("\r\nНажмите любую клавишу для завершения работы");
+                System.Console.ReadKey(true);
+                return;
+            }
+
+            //получаем значение размера квадрата
+            System.Console.WriteLine("Введите размер квадрата (не больше {0}): ", tableSize);
+            if (!int.TryParse(System.Console.ReadLine(), out squreSize)) return;
+
+            //проверяем что квадрат не больше таблицы
+            if(tableSize<squreSize)
+            {
+                System.Console.WriteLine("Размер квадрата не должен быть больше размера таблицы!");
+
+                System.Console.WriteLine("\r\nНажмите любую клавишу для завершения работы");
+                System.Console.ReadKey(true);
+                return; 
+            }
+
+            int[,] massiv=new int[tableSize,tableSize];
+
+            //Заполняем массив
+            System.Random random = new System.Random();
+            for (int x = 1; x < tableSize; x++)
+            {
+                for (int y = 1; y < tableSize; y++)
+                {
+                    massiv[x,y] = random.Next(10); 
+                }
+            }
+
+            //выбор квадрата
+            int nomer = 1;
+            for (int x = 0; x < tableSize - squreSize+1; x++)
+            {
+                for (int y = 0; y < tableSize - squreSize+1; y++)
+                {
+                    //расчет суммы квадрата
+                    int sum = 0;
+                    for (int k = x; k < squreSize + x; k++)
+                    {
+                        for (int l = y; l < squreSize + y; l++)
+                        {
+                            sum += massiv[k, l];
+                        }
+                    }
+                    System.Console.WriteLine("Сумма квадрата {0} равна {1}",nomer,sum);
+                    nomer++;
+                }
+            }
+            System.Console.WriteLine("\r\nНажмите любую клавишу для завершения работы");
+            System.Console.ReadKey(true);
+        }
+    }
+}
