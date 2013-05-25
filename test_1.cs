e881675e46858c29481c53d61074ac407f2f55c9
Alex Skiffin <alex@skiffin.ru>
2013-05-25 15:15:34 +0600

    доисправил 1

--- test_1.cs
+++ test_1.cs
@@ -1,4 +1,10 @@
-﻿namespace test_1
+﻿﻿/*
+Дан неубывающий массив неотрицательных целых чисел a[1]<=a[2]<=…<=a[n], n<=10000. 
+Найти наименьшее целое положительное число, не представимое в виде суммы нескольких 
+элементов этого массива (каждый элемент массива может быть использован не более одного раза). 
+*/
+
+namespace test_1
 {
     internal class Program
     {
@@ -20,11 +26,13 @@
 
             //заполняем массив случайными числами
             System.Random random = new System.Random();
-            for (int i = 1; i < razmer; i++)
+            for (int i = 0; i < razmer; i++)
             {
-                massiv[i] = massiv[i == 0 ? 0 : i - 1] + random.Next(10) + 1; //шаг небольшой, но для примера хватит
+                massiv[i] = massiv[i == 0 ? 0 : i - 1] + random.Next(10); //шаг небольшой, но для примера хватит
             }
 
+            //тестовый массив. ответ - 26
+            //int[] massiv = new[] { 0, 1, 2, 4, 5, 6, 7, 27, 33 };
             //выводим результат
             System.Console.WriteLine(Razchet(massiv) + "\r\nНажмите любую клавишу для закрытия");
             System.Console.ReadKey(true);
@@ -50,7 +58,7 @@
                 
             }
             //ищем минимальное значение
-            for (int i = 1; i < vhod.Length; i++)
+            for (int i = 1; i < (vhod.Length<<vhod.Length); i++)
             {
                 if (!can[i])
                 {
