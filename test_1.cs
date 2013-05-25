185b921d3f26e4253e58addeca205b3b0c3a5495
Alex Skiffin <alex@skiffin.ru>
2013-05-25 12:45:15 +0600

    исправил 1
    Происхдит поиск минималного значения как такового, а не из массива

--- test_1.cs
+++ test_1.cs
@@ -1,17 +1,11 @@
-﻿/*
-Дан неубывающий массив неотрицательных целых чисел a[1]<=a[2]<=…<=a[n], n<=10000. 
-Найти наименьшее целое положительное число, не представимое в виде суммы нескольких 
-элементов этого массива (каждый элемент массива может быть использован не более одного раза). 
-*/
-
-namespace test_1
+﻿namespace test_1
 {
-    class Program
+    internal class Program
     {
-        static void Main()
+        private static void Main()
         {
             //получаем размер массива
-            System.Console.WriteLine("Укажите размер массива (цифрами):");            
+            System.Console.WriteLine("Укажите размер массива (цифрами):");
             int razmer;
 
             //проверяем вводимые данные
@@ -26,9 +20,9 @@ namespace test_1
 
             //заполняем массив случайными числами
             System.Random random = new System.Random();
-            for (int i = 1; i < razmer;i++ )
+            for (int i = 1; i < razmer; i++)
             {
-                massiv[i] = massiv[i==0 ? 0 : i-1] + random.Next(10)+1;   //шаг небольшой, но для примера хватит
+                massiv[i] = massiv[i == 0 ? 0 : i - 1] + random.Next(10) + 1; //шаг небольшой, но для примера хватит
             }
 
             //выводим результат
@@ -39,7 +33,7 @@ namespace test_1
         //расчет и поиск минимиального значения
         public static string Razchet(int[] vhod)
         {
-            bool[] can = new bool[vhod.Length];
+            bool[] can = new bool[(vhod.Length<<vhod.Length)+1];
 
             //используем битовый сдвиг
             for (int i = 0; i < ((1 << vhod.Length) - 1); i++)
@@ -47,25 +41,27 @@ namespace test_1
                 int sum = 0;
                 for (int j = 0; j < vhod.Length; j++)
                 {
-                    sum += ProvBit(i, j) * vhod[j];
-                    if (sum > 0)
-                    {
-                        can[j] = true;
-                    }
+                    sum += ProvBit(i, j)*vhod[j];
                 }
+                if (sum > 0)
+                    {
+                        can[sum] = true;
+                    }
+                
             }
             //ищем минимальное значение
-            for (int i = 0; i < vhod.Length; i++)
+            for (int i = 1; i < vhod.Length; i++)
             {
-                if (can[i])
+                if (!can[i])
                 {
-                    return vhod[i].ToString();
+                    return i.ToString();
                 }
             }
             return "Нет такого числа\r\n";
         }
-		//проверка бита
-        static int ProvBit(int n, int i)
+
+        //проверка бита
+        private static int ProvBit(int n, int i)
         {
             return (n & (1 << i)) != 0 ? 1 : 0;
         }
